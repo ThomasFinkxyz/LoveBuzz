6710b9b468f645ca0c97a5d6c88ef63bec98d572
Thomas Fink <tomfink96@gmail.com>
2020-09-07 04:01:39 -0400

    Added ability to write to registers.

--- main.cpp
+++ main.cpp
@@ -211,32 +211,6 @@ uint64_t readReg(pid_t id, int dwarfNum){
 			return 0;
 	}
 
-//r13,13
-//r12,12
-//rbp,6
-//rbx,3
-//r11,11
-//r10,10
-//r9,9
-//r8,8
-//rax,0
-//rcx,2
-//rdx,1
-//rsi,4
-//rdi,5
-//orig_rax-2
-//rip,-1
-//cs,51},
-//eflags,49
-//rsp,7
-//ss,52,
-//fs_base,58
-//gs_base,59
-//ds,53
-//es,50
-//fs,54
-//gs,55
-
 }
 
 uint64_t readReg(pid_t id, std::string name){
@@ -261,10 +235,108 @@ void printRegisters(pid_t id){
 	regs.ds,regs.es,regs.fs,regs.gs);
 }
 
+// This is ugly, long and almost a copy and paste of the other function. But at least a big switch
+// is easy to understand. Can't think of a better way to do it.
+void writeReg(pid_t id, int dwarfNum, uint64_t value){
+	user_regs_struct regs;
+	ptrace(PTRACE_GETREGS,id,nullptr,&regs);
+	switch(dwarfNum){
+		case 15:
+			regs.r15 = value;
+			break;
+		case 14:
+			regs.r14 = value;
+			break;
+		case 13:
+			regs.r13 = value;
+			break;
+		case 12:
+			regs.r12 = value;
+			break;
+		case 6:
+			regs.rbp = value;
+			break;
+		case 3:
+			regs.rbx = value;
+			break;
+		case 11:
+			regs.r11 = value;
+			break;
+		case 10:
+			regs.r10 = value;
+			break;
+		case 9:
+			regs.r9 = value;
+			break;
+		case 8:
+			regs.r8 = value;
+			break;
+		case 0:
+			regs.rax = value;
+			break;
+		case 2:
+			regs.rcx = value;
+			break;
+		case 1:
+			regs.rdx = value;
+			break;
+		case 4:
+			regs.rsi = value;
+			break;
+		case 5:
+			regs.rdi = value;
+			break;
+		case -2:
+			regs.orig_rax = value;
+			break;
+		case -1:
+			regs.rip = value;
+			break;
+		case 51:
+			regs.cs = value;
+			break;
+		case 49:
+			regs.eflags = value;
+			break;
+		case 7:
+			regs.rsp = value;
+			break;
+		case 52:
+			regs.ss = value;
+			break;
+		case 58:
+			regs.fs_base = value;
+			break;
+		case 59:
+			regs.gs_base = value;
+			break;
+		case 53:
+			regs.ds = value;
+			break;
+		case 50:
+			regs.es = value;
+			break;
+		case 54:
+			regs.fs = value;
+			break;
+		case 55:
+			regs.gs = value;
+			break;
+		default:
+			printf("Error: No register with Dwarf num of %i \n",dwarfNum);
+	}
+	ptrace(PTRACE_SETREGS,id,nullptr,&regs);
+}
+
+
+void writeReg(pid_t id,std::string name,uint64_t value){
+	writeReg(id,regName2Num[name], value);
+}
 
 
 void debug(char name[], pid_t debugeeID){
 	wait(debugeeID);
+	printf("Debugee process id: %i \n",debugeeID);
     char* line = nullptr;
     while((line = linenoise("<debugger> ")) != nullptr) {
 		if(line[0] != '\0'){
@@ -280,10 +352,18 @@ void debug(char name[], pid_t debugeeID){
 				step(debugeeID);
 			}else if(is_prefix(command, "test")){
 				test(debugeeID);
-			}else if (is_prefix(command, "regs")){
+			}else if (is_prefix(command, "dump")){
 				printRegisters(debugeeID);
 				printf("rip is %x \n",readReg(debugeeID,"rip"));
-			} else{
+			} else if (is_prefix(command, "reg")){
+					if(args[2] == "read"){
+						printf("%x \n",readReg(debugeeID,args[1]));
+					} else if(args[2] == "write"){
+						writeReg(debugeeID,args[1],std::stoul(args[3],0,16));
+					}else{
+						printf("Unkown comman \n");
+					}
+			}else {
     			printf("Unknown command\n");
 			}
     		linenoiseHistoryAdd(line);
